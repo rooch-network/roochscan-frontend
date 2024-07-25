aa90d3d506bfc799c5a6bb2a816a915dee9bad2b
xiaolong <longjiuwei999@163.com>
2024-07-25 16:38:39 +0800

    add hooks

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,23 @@
 import server from "./server"
-import { BlockDetail, Transaction } from '@/types'
+import useStore from "@/store"
+interface IResponse<T> {
+  id: number,
+  jsonrpc: string,
+  result: T
+}
+const generatorParams = (id: number, method: string, params = [null, null]) => (
+  {
+    id,
+    jsonrpc: "2.0",
+    method,
+    params
+  }
+)
 
 export const queryBlockList = () => {
-  return server.post("/")
+  return server.post(useStore.getState().roochNodeUrl)
+}
+
+export const queyChainID = (): Promise<IResponse<string>> => {
+  return server.post(useStore.getState().roochNodeUrl, generatorParams(0, 'rooch_getChainID'))
 }
