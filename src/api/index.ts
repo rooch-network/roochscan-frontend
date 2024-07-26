a4d0aac883d6e68980e0f4248e0fc45b8dbd2c4d
xiaolong <longjiuwei999@163.com>
2024-07-26 15:38:47 +0800

    add query blanace api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import { IResponse, ITransactionsByOrderResponse } from "@/types"
 import server from "./server"
 import useStore from "@/store"
-const generatorParams = (method: string, params = [null, null], id = Math.floor(Math.random() * 400)) => (
+const generatorParams = (method: string, params: any[] = [null, null], id = Math.floor(Math.random() * 400)) => (
   {
     id,
     jsonrpc: "2.0",
@@ -16,4 +16,12 @@ export const queryBlockList = (): Promise<IResponse<{ data: ITransactionsByOrder
 
 export const queyChainID = (): Promise<IResponse<string>> => {
   return server.post(useStore.getState().roochNodeUrl, generatorParams('rooch_getChainID'))
+}
+
+export const queryBalances = (address: string) => {
+  return server.post(useStore.getState().roochNodeUrl, generatorParams('rooch_getBalances', [address, null, '5']))
+}
+
+export const queryBalance = (address: string) => {
+  return server.post(useStore.getState().roochNodeUrl, generatorParams('rooch_getBalance', [address, '0x3::gas_coin::GasCoin']))
 }
