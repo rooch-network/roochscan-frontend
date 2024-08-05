92f50db300c459185040258ab6a1ffc78d0e71df
xiaolong <longjiuwei999@163.com>
2024-08-05 10:06:46 +0800

    add api

--- src/api/index.ts
+++ src/api/index.ts
@@ -25,3 +25,20 @@ export const queryBalances = (address: string) => {
 export const queryBalance = (address: string) => {
   return server.post(useStore.getState().roochNodeUrl, generatorParams('rooch_getBalance', [address, '0x3::gas_coin::GasCoin']))
 }
+
+export const queryGetStatus = () => {
+  const params = ["/object/0xd9858821a52538c99f822d3f90ec798f76466bb7a1e82ebdb42d19b62a030069::quick_start_object_counter::Counter", { "decode": true }]
+  return server.post(useStore.getState().roochNodeUrl, generatorParams('rooch_getStates', params))
+}
+
+export const getTransactionsByHash = () => {
+  const params = [['0x08156186e176ae50d5e15cd52fd1225089621eef716dbb05736422ed1be58f8a']]
+  return server.post(useStore.getState().roochNodeUrl, generatorParams('rooch_getTransactionsByHash', params))
+}
+
+export const queryEvents = () => {
+  const params = [
+    { "sender": "0xd9858821a52538c99f822d3f90ec798f76466bb7a1e82ebdb42d19b62a030069" }, null, "10", true
+  ]
+  return server.post(useStore.getState().roochNodeUrl, generatorParams('rooch_queryEvents', params))
+}
