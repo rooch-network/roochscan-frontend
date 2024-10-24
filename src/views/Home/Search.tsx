ad1bf2f654b6b263f48495fa9836a17ad2833046
patricksssa78 <patricksssa78@gmail.com>
2024-10-24 14:31:23 +0800

    add styles

--- src/views/Home/Search.tsx
+++ src/views/Home/Search.tsx
@@ -35,46 +35,34 @@ export default function Home() {
 
         <Row gutter={16} className=" mt-[40px]">
           <Col span={6}>
-            <Card bordered={false}>
+            <Card bordered={false} style={{ backgroundColor: "#f4f4f5" }}>
               <Statistic title="Blocks" value={107523} precision={2} />
             </Card>
           </Col>
           <Col span={6}>
-            <Card bordered={false}>
+            <Card bordered={false} style={{ backgroundColor: "#f4f4f5" }}>
               <Statistic title="Transactions" value={112893} precision={2} />
             </Card>
           </Col>
           <Col span={6}>
-            <Card bordered={false}>
+            <Card bordered={false} style={{ backgroundColor: "#f4f4f5" }}>
               <Statistic title="Addresses" value={10752264} precision={2} />
             </Card>
           </Col>
           <Col span={6}>
-            <Card bordered={false}>
+            <Card bordered={false} style={{ backgroundColor: "#f4f4f5" }}>
               <Statistic title="Tps" value={20} precision={0} suffix="/sec" />
             </Card>
           </Col>
         </Row>
-        <div className="h-50 w-2/4 bg-white rounded-md flex items-center p-4 mt-40">
-          <input
+        <input
             onKeyPress={handleEnter}
             value={val}
             onChange={handleInput}
             type="text"
-            className="flex-1 h-full appearance-none focus:outline-none focus:ring-2 focus:ring-light-gray focus:border-light-gray pl-20 pr-10 rounded-md"
+            className="h-50 w-full bg-white border border-[#e4e4e7] flex items-center p-4 mt-40  focus:outline-none focus:ring-2 focus:ring-light-gray focus:border-light-gray pl-20 pr-10 rounded-md"
             placeholder="Search by Address / Txn Hash / Block / Token / Domain Name"
           />
-          <Select
-            defaultValue="hash"
-            style={{ width: 120 }}
-            value={select}
-            onChange={handleChange}
-            options={[
-              { value: "assets", label: "资产查询" },
-              { value: "hash", label: "哈希查询" },
-            ]}
-          />
-        </div>
       </div>
     </section>
   );
