f74d5218c65330e4322eb7cd3f6c6285e90c6b29
xink <zyhh1611054604@gmail.com>
2024-10-25 17:15:51 +0800

    feat:query object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+'use client'
+import React, {useEffect} from "react";
 import Search from "@/views/Home/Search";
 import DataView from "@/views/Home/DataView";
+import {getObjectById} from "@/api";
 
 
 export default function Home() {
+
+  useEffect(()=>{
+    (async ()=>{
+      let objId = "0x018316bdaf22b346fcbe43dab84faab08d039cf753a08342164150afcab718c1d2"
+      const res = await getObjectById(objId);
+      console.log(objId,  res, "getObject res")
+    })()
+  }, [])
 
     return <main className=" mt-[70px]">
         <Search />
