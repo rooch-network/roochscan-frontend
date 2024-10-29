ecf7bcd2f7f35a714920d21bc7562441a7ad51db
xink <zyhh1611054604@gmail.com>
2024-10-29 14:27:16 +0800

    fix:add method call

--- src/app/module/[id]/methodCall.tsx
+++ src/app/module/[id]/methodCall.tsx
@@ -2,6 +2,8 @@ import {FunctionDetail, IModule} from "@/types";
 import {Button, Form, Input, message} from "antd";
 import {useCreateSessionKey, useCurrentSession} from "@roochnetwork/rooch-sdk-kit";
 import {useState} from "react";
+import {RoochClient, Transaction} from "@roochnetwork/rooch-sdk";
+import useStore from "@/store";
 
 
 const MethodCall = ({moduleDetail, func}:{
@@ -11,6 +13,7 @@ const MethodCall = ({moduleDetail, func}:{
   const sessionKey = useCurrentSession()
   const { mutateAsync: createSessionKey } = useCreateSessionKey();
   const [form] = Form.useForm()
+  const [form1] = Form.useForm()
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () =>{
@@ -37,44 +40,86 @@ const MethodCall = ({moduleDetail, func}:{
         }
       }
 
-      const values = form.getFieldsValue();
+      const params = Object.values(form.getFieldsValue());
+      const typeParams = Object.values(form1.getFieldsValue());
+
+      console.log(params, typeParams)
+
+      const tx = new Transaction();
       const f = `${moduleDetail.address + "::" + moduleDetail.name}::${func.name}`
-      console.log(f, "fff")
-      console.log(sessionKey, "sessionKey")
-      sessionKey?.sendTransaction(f, [], [], { maxGasAmount: 1000000 }).finally(() => {
-        setLoading(false)
+      tx.callFunction({
+        target: f,
+        args: [...params],
       })
+      const client = new RoochClient({url:useStore.getState().roochNodeUrl});
+      const result = await client.signAndExecuteTransaction({
+        transaction: tx,
+        signer: sessionKey,
+      })
+
+      if(result.execution_info.status.type === 'executed'){
+        message.success("Executed")
+      }
     } catch (e){
+      console.log(e, "Error")
       setLoading(false)
       message.error(e.message)
     }
   }
 
-  return <Form
-    form={form}
-    className={"w-[700px]"}>
-
-    {
-      func.params.map(item =>{
-        return  <Form.Item
-          key={item}
-          name={item}
-          label={item}
-          rules={[
+  return <div>
+    <div className={"flex"}>
+      <Form
+        form={form}
+        className={"w-[400px]"}>
+        <div>
+          {
+            func.params.map(item =>{
+              return  <Form.Item
+                key={item}
+                name={item}
+                label={item}
+                rules={[
+                  {
+                    required: true,
+                  },
+                ]}
+              >
+                <Input placeholder={item} key={item}></Input>
+              </Form.Item>
+            })
+          }
+        </div>
+      </Form>
+      <Form
+        form={form1}
+        className={"w-[400px]"}>
+        <div className={"flex justify-end"}>
+          <div>
             {
-              required: true,
-            },
-          ]}
-        >
-          <Input placeholder={item} key={item}></Input>
-        </Form.Item>
-      })
-    }
-
-    <div className={"flex justify-end"}>
+              func.type_params[0].constraints.map(item =>{
+                return  <Form.Item
+                  key={item}
+                  name={item}
+                  label={item}
+                  rules={[
+                    {
+                      required: true,
+                    },
+                  ]}
+                >
+                  <Input placeholder={item} key={item}></Input>
+                </Form.Item>
+              })
+            }
+          </div>
+        </div>
+      </Form>
+    </div>
+    <div className={"flex justify-center"}>
       <Button loading={loading} onClick={handleSubmit} type={"primary"}>Submit</Button>
     </div>
-  </Form>
+  </div>
 }
 
 export default MethodCall;
