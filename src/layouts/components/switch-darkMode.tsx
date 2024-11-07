debdba9c18355c1ca95cda828906b8dccb290c1e
patricksssa78 <patricksssa78@gmail.com>
2024-11-07 15:34:09 +0800

    fix style

--- src/layouts/components/switch-darkMode.tsx
+++ src/layouts/components/switch-darkMode.tsx
@@ -6,30 +6,25 @@ import * as React from 'react';
 
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import { useColorScheme } from '@mui/material';
+import { Button, useColorScheme } from '@mui/material';
 import {WbSunnyOutlined,DarkModeOutlined} from '@mui/icons-material';
 
 export function SwitchDorkMode() {
   const { mode, setMode } = useColorScheme();
-  const handleChange = (event: SelectChangeEvent) => {
-    setMode(event.target.value as any);
+  const handleChange = () => {
+    if(mode ==='dark') {
+        setMode('light')
+    }else {
+      setMode('dark')
+    }
   };
 
   return (
-    <div>
-      <Select
-        value={mode}
-        size="small"
-        onChange={handleChange}
-        displayEmpty
-      >
-        <MenuItem value="light">
-          <WbSunnyOutlined  fontSize='small'  />
-        </MenuItem>
-        <MenuItem value="dark">
-          <DarkModeOutlined  fontSize='small'  />
-        </MenuItem>
-      </Select>
-    </div>
-  );
+    <Button
+    className="w-fit"
+    onClick={handleChange}
+  >
+      {mode === 'dark' ? <DarkModeOutlined  fontSize='small'  /> : <WbSunnyOutlined  fontSize='small'  />}
+  </Button>
+  )
 }
