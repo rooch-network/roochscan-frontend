ffc7be9cacbdc563cb0193ca01de8f75cc3eb5f7
YusongWang <admin@wangyusong.com>
2024-11-10 20:50:35 -0900

    change home by default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
 'use client';
 
-import { useEffect } from 'react';
-
-import { paths } from 'src/routes/paths';
-import { useRouter } from 'src/routes/hooks';
+import SearchView from "src/sections/search/overview";
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push(paths.dashboard.search);
-  }, [router]);
-
-  return null;
+  return <SearchView />;
 }
--- src/app/search/page.tsx
+++
@@ -1,7 +0,0 @@
-import SearchView from 'src/sections/search/overview';
-
-export const metadata = { title: `Search Account` };
-
-export default function Page() {
-  return <SearchView />;
-}
